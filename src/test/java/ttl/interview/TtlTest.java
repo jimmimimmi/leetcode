296b4055a53d454a0b9cdeaaf622b8ad1555eb6e
jimmimimmi <alex.g.gridnev@gmail.com>
2023-03-21 21:55:28 +0200

    test commit

--- src/test/java/ttl/interview/TtlTest.java
+++ src/test/java/ttl/interview/TtlTest.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class TtlTest extends TestCase {
     public void testName() {
         var result1 = new Ttl().findWord(List.of("W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"));
-//        var result2 = new Ttl().getLargestPiece("100,70", List.of(80, 20), List.of(35));
+
+        //        var result2 = new Ttl().getLargestPiece("100,70", List.of(80, 20), List.of(35));
 //        var result3 = new Ttl().getLargestPiece("100,70", List.of(80, 20), List.of(35));
 //        var result4 = new Ttl().getLargestPiece("100,70", List.of(80, 20), List.of(35));
 //        var result5 = new Ttl().getLargestPiece("100,70", List.of(80, 20), List.of(35));
