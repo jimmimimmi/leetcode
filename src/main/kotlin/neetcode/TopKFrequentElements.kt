4ce7428f44626b650fd486b5d3fd4f479387f8bc
jimmimimmi <alex.g.gridnev@gmail.com>
2024-06-20 21:03:55 +0300

    neetcode TopKFrequentElements

---
+++ src/main/kotlin/neetcode/TopKFrequentElements.kt
@@ -0,0 +1,35 @@
+package neetcode
+
+// https://leetcode.com/problems/top-k-frequent-elements/
+class TopKFrequentElements {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val freqMap = mutableMapOf<Int, Int>()
+        nums.forEach {
+            val prevVal = freqMap[it]
+            freqMap[it] = (prevVal ?: 0) + 1
+        }
+
+        val allPossibleFreqArray = Array<MutableList<Int>>(nums.size) { mutableListOf() }
+
+        freqMap.forEach { (num, freq) ->
+            allPossibleFreqArray[freq - 1].add(num)
+        }
+
+        val result = mutableListOf<Int>()
+
+        for (i in allPossibleFreqArray.size - 1 downTo 0) {
+            for (num in allPossibleFreqArray[i]) {
+                if (result.size < k) {
+                    result.add(num)
+                } else {
+                    return result.toIntArray()
+                }
+            }
+
+        }
+
+
+        return result.toIntArray()
+
+    }
+}
